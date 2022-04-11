292d731012e9920192f459e6d1ec4f154906ee2d
Aniruddha <aniruddha1712@gmail.com>
2022-04-11 16:53:43 +0530

    [Aniruddha]Added: Created binary search tree

--- BinarySearchTree/Program.cs
+++ BinarySearchTree/Program.cs
@@ -11,6 +11,12 @@ namespace BinarySearchTree
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to the Binary Search Tree\n");
+
+            MyBinarySearchTree<int> binarySearchTree = new MyBinarySearchTree<int>(56);
+            binarySearchTree.Insert(30);
+            binarySearchTree.Insert(70);
+            binarySearchTree.Display();
+
             Console.ReadLine();
         }
     }
